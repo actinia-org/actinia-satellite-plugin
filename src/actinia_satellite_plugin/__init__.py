0d7ef65eb2493fa0689d9c40c9d386700d91cb08
Carmen <carmen.tawalika@gmail.com>
2024-06-18 18:03:28 +0200

    fix import error

--- src/actinia_satellite_plugin/__init__.py
+++ src/actinia_satellite_plugin/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+import importlib.metadata
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
+    dist_name = 'actinia_metadata_plugin.wsgi'
+    __version__ = importlib.metadata.version(dist_name)
+except Exception:
+    __version__ = 'unknown'
