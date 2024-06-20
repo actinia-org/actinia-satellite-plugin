dbaabc4c22bc15b99076c888fdf0b3f517227eaf
Carmen Tawalika <mmacata@users.noreply.github.com>
2024-06-20 13:57:39 +0200

    Update __init__.py

--- src/actinia_satellite_plugin/__init__.py
+++ src/actinia_satellite_plugin/__init__.py
@@ -3,7 +3,7 @@ import importlib.metadata
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = 'actinia_satellite_plugin.wsgi'
+    dist_name = __name__
     __version__ = importlib.metadata.version(dist_name)
 except Exception:
-    __version__ = 'unknown'
+    __version__ = "unknown"
