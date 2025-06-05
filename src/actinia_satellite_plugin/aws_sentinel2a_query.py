0cc90756312666de2e7f5afb75cb4dcbc7f14156
Carmen <carmen.tawalika@gmail.com>
2025-06-05 18:05:09 +0200

    adjust imports

--- src/actinia_satellite_plugin/aws_sentinel2a_query.py
+++ src/actinia_satellite_plugin/aws_sentinel2a_query.py
@@ -10,7 +10,7 @@ from actinia_core.models.response_models import SimpleResponseModel
 from actinia_core.core.common.aws_sentinel_interface import (
     AWSSentinel2AInterface,
 )
-from actinia_core.rest.base.resource_base import ResourceBase
+from actinia_rest_lib.resource_base import ResourceBase
 from flask_restful_swagger_2 import swagger, Schema
 from copy import deepcopy
 
